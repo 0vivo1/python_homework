cf1c4935be2c662e2bd10bed092dba288dbf31b3
0vivo1 <85081701+0vivo1@users.noreply.github.com>
2021-08-20 20:05:20 +0300

    Update 1.2.py
    Добавлен выбод через f-строки

--- 1.2.py
+++ 1.2.py
@@ -2,4 +2,4 @@ user_time = int(input("Введите время в секундах : "))
 seconds = user_time % 60
 minutes = (user_time // 60) % 60
 hours = user_time // 3600
-print(hours, ':', minutes, ':', seconds)
+print(f'{hours:02}:{minutes:02}:{seconds:02})
